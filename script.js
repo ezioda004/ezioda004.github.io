20ba0c87e14b62099daa2d6a98e655d8ee73b062
Aditya Verma <adi004.zz@gmail.com>
2018-02-05 18:36:10 +0530

    Added an intro using typed.js

--- script.js
+++ script.js
@@ -2,7 +2,35 @@ $(document).ready(function() {
  
     setTimeout(function(){
         $('body').addClass('loaded');
-        $('h1').css('color','#222222');
-    }, 3000);
+        
+        
+        var options = {
+            strings: ["Hello World;"],
+            typeSpeed: 120,
+            startDelay: 1000,
+            backDelay: 1000,
+            backSpeed: 200,
+            smartBackspace: true,
+            showCursor: false,
+            cursorChar: '|',
+            // fadeOutClass: 'typed-fade-out'
+            onComplete: (self) => {var typed2 = new Typed("h2", second);},
+          }
+
+          var second = {
+            strings: ["I'm Aditya. ^1000", "A Developer. ^1000", "A Dreamer. ^1000" , "A Programmer."],
+            typeSpeed: 120,
+            startDelay: 1000,
+            backDelay: 1000,
+            backSpeed: 200,
+            smartBackspace: true,
+            showCursor: true,
+            cursorChar: '|',
+          }
+          
+          var typed = new Typed("h1", options);
+    }, 4500);
  
+
+    
 });
